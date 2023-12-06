b00197d083c8039a6fdae3c2aa6c72ab5a3c32a6
srinath-T-design <srinaththiyagarajan707@gmail.com>
2023-12-06 21:01:15 +0530

    remove one line

--- ConsoleApp_withGitHub/Program.cs
+++ ConsoleApp_withGitHub/Program.cs
@@ -3,4 +3,3 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello 12345");
 Console.WriteLine("hello war");
 Console.WriteLine("hello world war 1");
-Console.WriteLine("hello world war 2");
