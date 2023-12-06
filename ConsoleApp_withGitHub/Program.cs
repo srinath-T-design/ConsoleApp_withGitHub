2f513111f10e041018aef4d2daffc41eb7e72671
srinath-T-design <srinaththiyagarajan707@gmail.com>
2023-12-06 21:14:34 +0530

    remove 2 lines

--- ConsoleApp_withGitHub/Program.cs
+++ ConsoleApp_withGitHub/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello 12345");
-Console.WriteLine("hello war");
-Console.WriteLine("hello world war 1");
+
